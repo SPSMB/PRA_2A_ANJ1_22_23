5b3a74e97b996b04fd71c4f0107c863717b78b1b
frafticekcz <90890891+frafticekcz@users.noreply.github.com>
2022-10-17 14:41:56 +0200

    Add files via upload

---
+++ PRA_17.10/HodnoticiSystem.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HodnoticiSystem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Scanner, získáváme vstup z konzole
+        System.out.println("Zadej úspěšnost od 0 do 100"); //Ptáme se uživatele na hodnotu
+        int x = sc.nextInt(); //Hodnotu od uživatele uložíme do proměnné
+        if (x <= 19) { //Podmínka - jestliže je X menší nebo rovno 19 => známka je rovna 5
+            System.out.println("Známka: 5");
+        } else if (x <= 39) { //Podmínka - jestliže je X menší nebo rovno 39 => známka je rovna 4
+            System.out.println("Známka: 4");
+        } else if (x <= 59) { //Podmínka - jestliže je X menší nebo rovno 59 => známka je rovna 3
+            System.out.println("Známka: 3");
+        } else if (x <= 79) { //Podmínka - jestliže je X menší nebo rovno 79 => známka je rovna 2
+            System.out.println("Známka: 2");
+        } else { //Podmínka - jestli X NENÍ menší než 19, 39, 59 a 79 => známka je rovna 1
+            System.out.println("Známka: 1");
+        }
+    }
+}
